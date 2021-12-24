d8ce54fe998612a72901926d2213f58aa14cb5fd
Soobinhand <chelsea9528@naver.com>
2021-12-24 21:12:03 +0900

    두 행렬의 곱을 구하는 솔루션 수정

--- src/Algorithm/Matrix_Multiplication.java
+++ src/Algorithm/Matrix_Multiplication.java
@@ -7,10 +7,10 @@ import java.util.Arrays;
 public class Matrix_Multiplication {
 
     public static int[][] matrix_multiplication(int[][] a, int[][] b){
-        int[][] answer = new int[a.length][b.length];
+        int[][] answer = new int[a.length][b[0].length];
         for (int i =0; i<a.length;i++){
-            for(int j=0;j<b.length;j++){
-                for (int k=0;k<a.length;k++){
+            for(int j=0;j<b[0].length;j++){
+                for (int k=0;k<a[0].length;k++){
                     answer[i][j] += a[i][k] * b[k][j];
                 }
             }
