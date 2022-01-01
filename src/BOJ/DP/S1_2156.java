71cafe6bd9f1efd8ee553cd9ed59e5637d1d4034
Soobinhand <chelsea9528@naver.com>
2022-01-01 18:19:02 +0900

    백준 <S1_2156 포도주 시식> 해결

---
+++ src/BOJ/DP/S1_2156.java
@@ -0,0 +1,23 @@
+package BOJ.DP;
+
+import java.util.Scanner;
+
+public class S1_2156 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] arr = new int[a];
+        for (int i=0;i<a;i++){
+            arr[i] = sc.nextInt();
+        }
+        int[] dp = new int[a];
+
+        if (a>=1) dp[0] = arr[0];
+        if (a>=2) dp[1] = arr[0] + arr[1];
+        if (a>=3) dp[2] = Math.max(dp[1],Math.max(dp[0]+arr[2],arr[1]+arr[2]));
+        for (int i=3;i<arr.length;i++){
+            dp[i] = Math.max(dp[i-1], Math.max(dp[i-2]+arr[i], dp[i-3] + arr[i-1]+arr[i]));
+        }
+        System.out.println(dp[a-1]);
+    }
+}
