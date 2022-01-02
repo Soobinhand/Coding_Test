30b22321b4586c28810912fd052251a38240b951
Soobinhand <chelsea9528@naver.com>
2022-01-02 17:34:48 +0900

    백준 템플릿, 프로그래머스 템플릿 생성

---
+++ src/BOJ/Main.java
@@ -0,0 +1,11 @@
+package BOJ;
+
+/**
+ * 백준 템플릿
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Programmers/Solution.java
@@ -0,0 +1,8 @@
+package Programmers;
+
+/**
+ * 프로그래머스 템플릿
+ */
+
+class Solution {
+}
