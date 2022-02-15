e64385845672b2ec02ce4498698175d67b3664cb
Soobinhand <chelsea9528@naver.com>
2022-02-15 22:28:51 +0900

    백준 <S3_15657 N과 M (8)> 해결

---
+++ src/BOJ/Back_Tracking/S3_15657.java
@@ -0,0 +1,37 @@
+package BOJ.Back_Tracking;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class S3_15657 {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        arr = new int[n];
+        answer = new int[n];
+        for (int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        dfs(0,0);
+    }
+    static void dfs(int depth, int start){
+        if (depth == m){
+            for (int i=0;i<m;i++){
+                System.out.print(answer[i] +" ");
+            }
+            System.out.println();
+            return;
+        }
+        for (int i=start;i<n;i++){
+            answer[depth] = arr[i];
+            dfs(depth+1, i);
+        }
+    }
+}
