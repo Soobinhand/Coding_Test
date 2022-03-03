da0abb88d8fc1b43609645cb9ffd2f64eb0a44b5
Soobinhand <chelsea9528@naver.com>
2022-03-03 23:12:13 +0900

    백준 <S4_10546 배부른 마라토너> 해결

---
+++ src/BOJ/HashMap/S4_10546.java
@@ -0,0 +1,33 @@
+package BOJ.HashMap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
+public class S4_10546 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        Map<String, Integer> map = new HashMap<>();
+        for (int i=0;i<a;i++){
+            String key = br.readLine();
+            if (map.get(key) == null){
+                map.put(key, 1);
+            }else{
+                map.put(key, map.get(key) + 1);
+            }
+        }
+        for(int i=0;i<a-1;i++){
+            String key = br.readLine();
+            map.put(key, map.get(key) - 1);
+        }
+        for (String key : map.keySet()){
+            if (map.get(key) != 0){
+                System.out.println(key);
+                break;
+            }
+        }
+    }
+}
