f1e08f417f5af82a9de883b327b07e8160fbeb27
Soobinhand <chelsea9528@naver.com>
2022-03-17 23:38:07 +0900

    백준 <B4_5532 방학 숙제> 해결

---
+++ src/BOJ/Math/B4_5532.java
@@ -0,0 +1,20 @@
+package BOJ.Math;
+
+import java.util.Scanner;
+
+public class B4_5532 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int L = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        double C = sc.nextDouble();
+        double D = sc.nextDouble();
+        int max;
+        int korean = (int) Math.ceil(A / C);
+        int math = (int) Math.ceil(B/D);
+        max = Math.max(korean, math);
+        int answer = L - max;
+        System.out.println(answer);
+    }
+}
