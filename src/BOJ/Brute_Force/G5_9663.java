9f8324cc5c07823bab0fdab917b91094ed81e2aa
Soobinhand <chelsea9528@naver.com>
2022-04-02 23:02:48 +0900

    백준 <G5_9663 N-Queen> 해결

--- src/BOJ/Brute_Force/G5_9663.java
+++ src/BOJ/Brute_Force/G5_9663.java
@@ -3,47 +3,39 @@ package BOJ.Brute_Force;
 import java.util.Scanner;
 
 public class G5_9663 {
-    static int n, answer;
-    static int[] col;
 
-    static boolean attackable(int r1, int c1, int r2, int c2){
-        if(c1 == c2) return true;
-        if(r1 - c1 == r2 - c2) return true;
-        if(r1 + c1 == r2 + c2) return true;
-        return false;
+    static int[] arr;
+    static int N;
+    static int count = 0;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        arr = new int[N];
+        nQueen(0);
+        System.out.println(count);
     }
 
-
-
-
-    static void rec_func(int row){
-        if(row == n+1){
-            answer++;
-        }else{
-            for (int i = 1;i<=n;i++){
-                boolean possible = true;
-                for(int j=1;j<=row-1;j++){
-                    if (attackable(row, i, j, col[j])){
-                        possible = false;
-                        break;
-                    }
-                }
-                if (possible) {
-
-
-                    col[row] = i;
-                    rec_func(row + 1);
-                    col[row] = 0;
-                }
+    static void nQueen(int depth){
+        if (depth == N){
+            count++;
+            return;
+        }
+        for (int i=0;i<N;i++){
+            if (Possibility(depth)){
+                nQueen(depth + 1);
             }
         }
     }
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        col = new int[n+1];
-        rec_func(1);
-        System.out.println(answer);
+    static boolean Possibility(int col){
+        for (int i=0;i<col;i++){
+            if (arr[col] == arr[i]){
+                return false;
+            }else if(Math.abs(col - i) == Math.abs(arr[col] - arr[i])){
+                return false;
+            }
+        }
+        return true;
     }
 }
