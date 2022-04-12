c0dddec9139c6504ad6e00a3cee3f65fb0bac6a7
Soobinhand <chelsea9528@naver.com>
2022-04-12 20:04:45 +0900

    백준 <B3_9085 더하기> 해결

---
+++ src/BOJ/Math/B3_9085.java
@@ -0,0 +1,18 @@
+package BOJ.Math;
+
+import java.util.Scanner;
+
+public class B3_9085 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int testCase = sc.nextInt();
+        for (int i=0;i<testCase;i++){
+            int n = sc.nextInt();
+            int sum = 0;
+            for (int j=0;j<n;j++){
+                sum += sc.nextInt();
+            }
+            System.out.println(sum);
+        }
+    }
+}
