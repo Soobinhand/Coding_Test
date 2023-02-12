04ea0e08c66a7f6601497ebb925f871a03a16cec
손수빈 <chelsea9528@naver.com>
2023-02-12 16:47:58 +0900

    백준 <G4_17298 오큰수> 해결

--- src/BOJ/Stack/G4_17298.java
+++ src/BOJ/Stack/G4_17298.java
@@ -1,26 +1,35 @@
 package BOJ.Stack;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.util.Arrays;
 import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class G4_17298 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
-        Stack<Integer> stack = new Stack<>();
-        int[] arr = new int[testCase];
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        for (int i=0;i<testCase;i++){
-            arr[i] = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(br.readLine()); // 4
+        int[] A = new int[n]; // 들어오는 값들 3 5 2 7
+        int[] answer = new int[n];
+        String[] strings = br.readLine().split(" "); // 3 5 2 7
+        for (int i = 0; i < n; i++){
+            A[i] = Integer.parseInt(strings[i]); // 3 5 2 7
         }
-        for (int i=0;i<testCase;i++){
-            while (!stack.empty() && arr[stack.peek()] < arr[i]){
-                arr[stack.pop()] = arr[i];
+        Stack<Integer> stack = new Stack<>();
+        stack.push(0);
+        for (int i = 1;i <n;i++){
+            while (!stack.isEmpty() && A[stack.peek()] < A[i]){
+                answer[stack.pop()] = A[i];
             }
             stack.push(i);
         }
+        while (!stack.isEmpty()){
+            answer[stack.pop()] = -1;
+        }
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        for (int i=0;i<n;i++){
+            bw.write(answer[i] + " ");
+        }
+        bw.flush();
     }
 }
